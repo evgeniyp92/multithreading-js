40ea53c1eea6fab0b8b63c1930e584e35e09f1dd
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-02-12 19:03:53 +0100

    Refactor worker thread communication using MessageChannel

--- ch-3-node/workerThreads.js
+++ ch-3-node/workerThreads.js
@@ -3,16 +3,34 @@
 // without changing this will not yield better results -- you'll be stacking
 // threads on threads
 
-// // bidirectional comms via default message port
-const { Worker, isMainThread, parentPort } = require('worker_threads');
+// bidirectional comms via MessagePort created with MessageChannel
+// This looks confusing because the messages are being sent in a circular fashion
+const {
+  Worker,
+  isMainThread,
+  MessageChannel,
+  workerData,
+} = require('worker_threads');
+
 if (isMainThread) {
-  const worker = new Worker(__filename);
-  worker.on('message', msg => {
-    worker.postMessage(msg);
+  const { port1, port2 } = new MessageChannel();
+
+  const worker = new Worker(__filename, {
+    workerData: { port: port2 }, // sets up port2 to be port in the worker
+    transferList: [port2], // gives over ownership of port2 to the worker
+    // ports that are transferred over cannot be used on the sending side anymore
+  });
+
+  port1.on('message', data => {
+    console.log('port 1 got a message: ', data);
+    port1.postMessage(data);
   });
 } else {
-  parentPort.on('message', msg => {
-    console.log('received message from parent: ', msg);
+  const { port } = workerData; // receiving the port (which is port2 in the side scope)
+  port.on('message', msg => {
+    console.log('got a message from main: ', msg);
   });
-  parentPort.postMessage('hello world');
+  port.postMessage('Hello world');
 }
+
+// TODO: Look into ReadableStream and WritableStream for communicating across threads
